26cb15cea9ba1522c6e998da3799aba222b5d5ec
VinhTranBa <Vinhtran.ytcc@gmail.com>
2025-05-28 09:47:32 +0700

    up
    up

--- day1/inclass.R
+++ day1/inclass.R
@@ -1,79 +1,98 @@
-## Read binary file
-library(readxl) ## Excel
-library(haven) ## STATA
+df <- life_exp
+?read.table()
+df <-read.table(file="data/life-exp.csv", header = T, sep =",")
+head(df)
+# data import
+library(readxl)
+library(haven)
+?readxl
+df2 <- read_csv("data/life-exp.csv", sheet=1)
+df3 <- read_dta("data/linelist-raw.dta")
 
-df <- read_excel(path = "data/vaccine_data.xlsx", 
-                 sheet = 1)
+# data clean
+a <- 2 # Giá trị
+a <- c(1:4) # Vector
+df <- data.frame(a = c(1:4),
+                 b = c(2:5)) # Matrix
+df <- readxl::read_xlsx("data/vaccine_data.xlsx")
+class(df)
 
-## 1. Có bao nhiêu biến có missing, gồm những loại biến nào?
+# bài tập
+# tải dữ liệu
+vaccine <- readxl::read_xlsx("data/vaccine_data.xlsx")
+#1
 library(skimr)
-skim(df)
-
-## 2. Làm sạch tên cột và đổi tên vgb_24 thành vgb_truoc_24
-## và vgb_24_2 thành vgb_sau_24
+is.na(vaccine)
+skimr::skim(vaccine)
+#2
 library(janitor)
 library(tidyverse)
-df <- df %>% clean_names() #%>% colnames()
 
-df <- df %>% 
+vaccine <- vaccine %>% 
   clean_names() %>% # Tự động làm sạch tên cột
   rename( 
     vgb_truoc_24 = vgb_24, # Tên mới = Tên cũ
     vgb_sau_24 = vgb_24_2
   )
-
-## 3. Đổi kiểu dữ liệu của các biến về đúng loại dữ liệu
+#3
+str(vaccine)
 
 date_cols <- c("ngaysinh", "vgb_truoc_24","vgb_sau_24","vgb_1","vgb_2","vgb_3","vgb_4","hg_1","hg_2","hg_3","hg_4","uv_1","uv_2","uv_3","uv_4")
 
-df <- df %>% 
+vaccine <- vaccine %>% 
   mutate( # Each column
     gioitinh = as.factor(gioitinh),
-    tinhtrang = ifelse(tinhtrang == "theo dõi", T, F)
+    tinhtrang = ifelse(tinhtrang == "theo dõi", TRUE, FALSE)
   ) %>% 
   mutate_at( # Multiple column
     date_cols, 
     dmy
-  ) %>% 
-  rename(
-    theodoi = tinhtrang
   )
 
-## 4. Lọc dữ liệu
-## * Những người có Quận 2.
-## * Những người có vgb_1 trước ngày 20 tháng 2 năm 2024.
-## * Những người có Quận 2 hoặc vgb_1 trước ngày 20 tháng 2 năm 2024.
-## * Những người có Quận 2 và vgb_1 trước ngày 20 tháng 2 năm 2024.
+str(vaccine)
+#4
+vaccine %>% filter(huyen == "Quận 2")
+vaccine %>% filter(vgb_1<as.Date("2024-02-20"))
+vaccine %>% filter(huyen == "Quận 2" | vgb_1<as.Date("2024-02-20"))
+vaccine %>% filter(huyen == "Quận 2" & vgb_1<as.Date("2024-02-20"))
 
-df %>% filter(huyen == "Quận 2") %>% select(id, huyen)
-
-df %>% filter(vgb_1 < as.Date("2024-02-20")) %>% select(id, vgb_1)
-
-df %>% filter(huyen == "Quận 2" | vgb_1 < as.Date("2024-02-20")) %>% select(id, huyen, vgb_1)
-
-df %>% filter(huyen == "Quận 2" & vgb_1 < as.Date("2024-02-20")) %>% select(id, huyen, vgb_1)
-
-## 5. Thay những giá trị NA của vgb_truoc_24 thành ngày hiện tại
+#5
+?replace_na()
+library(dplyr)
 library(lubridate)
-df <- df %>%
-  mutate(vgb_truoc_24 = replace_na(vgb_truoc_24, today()))
+is.na(vaccine$vgb_truoc_24)
 
-table(df$vgb_truoc_24==today())
+vaccine <-  vaccine %>% 
+  mutate(vgb_truoc_24= replace_na(vgb_truoc_24, today()))
 
-## 6. Tạo function để tính cho các quốc gia khác 
-
+vaccine <- vaccine %>%
+  mutate(vgb_truoc_24 = coalesce(vgb_truoc_24, today()))
+table(vaccine$vgb_truoc_24==today())
+#6
+# data import
 covid_cases <- readRDS("data/covid_cases.rds")
+# example
+case_col <- "cases_chn"
 
-compute_stats <- function(data, colname){
+basic_stats <- c(
+  min =  min(covid_cases[[case_col]]),
+  q1 = quantile(covid_cases[[case_col]], 0.25, names=FALSE),
+  median = median(covid_cases[[case_col]]),
+  q3 = quantile(covid_cases[[case_col]], 0.75, names=FALSE),
+  max = max(covid_cases[[case_col]])
+)
+# tạo function cho các quốc gia
+
+func_national <- function(data,colum) {
   return(
     c(
-      min = min(data[[colname]]),
-      q1 = quantile(data[[colname]], 0.25, names=FALSE),
-      median = median(data[[colname]]),
-      q3 = quantile(data[[colname]], 0.75, names=FALSE),
-      max = max(data[[colname]])
-    )
+    min = min(data[[colum]], na.rm = TRUE),
+    q1 = quantile(data[[colum]], 0.25, names = FALSE, na.rm = TRUE),
+    median = median(data[[colum]], na.rm = TRUE),
+    q3 = quantile(data[[colum]], 0.75, names = FALSE, na.rm = TRUE),
+    max = max(data[[colum]], na.rm = TRUE)
   )
+)
 }
-
-compute_stats(covid_cases, "cases_jpn")
+#chạy kết quả
+func_national(covid_cases, "cases_jpn")
